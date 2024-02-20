262a5bff5ef93225ec93636870ff626ef613b9e8
Jovana Matic <jovana.stankovic85@yahoo.com>
2024-02-20 11:22:45 +0100

    Implement error handling

--- app.vue
+++ app.vue
@@ -6,15 +6,21 @@
   }
   const inputValue = ref('')
   const background = ref('')
+  const cityError = ref(null)
   // const { data: city, error } = useFetch(() => `http://api.weatherapi.com/v1/current.json?key=96191023498a4f13b4f122145241902&q=${citySearch.value}&aqi=no`)
 
 
   const { data: city, error } = useAsyncData('city', async () => {
-
+    cityError.value = null
     let response
-    
     try {
-      response = await $fetch(`http://api.weatherapi.com/v1/current.json?key=96191023498a4f13b4f122145241902&q=${citySearch.value}&aqi=no`)
+      response = await $fetch(`http://api.weatherapi.com/v1/current.json?key=96191023498a4f13b4f122145241902`, {
+        params: {
+          aqi: 'no',
+          q: `${citySearch.value}`,
+        }
+      }
+      )
       cookie.value = citySearch.value
       const temp = response.current.temp_c
 
@@ -33,6 +39,7 @@
       }
     } catch (error) {
       console.log('Invalid city')
+      cityError.value = error
     }
     return response
   }, {
@@ -64,6 +71,9 @@
       <div class="mt-20">
         <input type="text" class="w-1/2 h-10" placeholder="Search a city" v-model="inputValue"/>
         <button class="bg-sky-400 w-20 text-white h-10" @click="submitCity()">Search</button>
+        <div v-if="cityError" class="text-white text-2xl">
+          <p>Invalid city</p>
+        </div>
       </div>
     </div>
   </div>
